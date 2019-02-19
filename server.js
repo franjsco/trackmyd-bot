3a5e51c5e288b29863699286d75f52d2cc8b2365
Francesco Esposito <33671357+frab1t@users.noreply.github.com>
2019-02-19 14:29:57 +0100

    add routing messages

--- server.js
+++ server.js
@@ -12,5 +12,25 @@ bot.on('/start', (msg) => {
   bot.sendMessage(msg.from.id, `hello ${name}`);
 });
 
+bot.on('/help', (msg) => {
+  bot.sendMessage(msg.from.id, 'help template');
+});
+
+bot.on('/list', (msg) => {
+  bot.sendMessage(msg.from.id, 'list template');
+});
+
+bot.on('/position', (msg) => {
+  bot.sendMessage(msg.from.id, 'position template');
+});
+
+bot.on('/add', (msg) => {
+  bot.sendMessage(msg.from.id, 'add');
+});
+
+bot.on('/remove', (msg) => {
+  bot.sendMessage(msg.from.id, 'remove');
+});
+
 
 bot.start();
